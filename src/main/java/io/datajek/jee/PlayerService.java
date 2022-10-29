f652953465ea3d26d9445e0ce4c63268fd718c46
codeForGood300 <vardhan2011harsh@gmail.com>
2022-10-29 19:21:17 +0530

    POST request

---
+++ src/main/java/io/datajek/jee/PlayerService.java
@@ -0,0 +1,2 @@
+package io.datajek.jee;public class PlayerService {
+}
--- src/main/java/io/datajek/jee/PlayerServlet.java
+++ src/main/java/io/datajek/jee/PlayerServlet.java
@@ -18,6 +18,19 @@ public class PlayerServlet extends HttpServlet {
             request.setAttribute("name", playerName);
             request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(request, response);
         }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String playerName = request.getParameter("name");
+
+        Player player = service.getPlayerByName(playerName);
+
+        request.setAttribute("name", playerName);
+        request.setAttribute("country", player.getNationality());
+        request.setAttribute("dob", player.getBirthDate());
+        request.setAttribute("titles", player.getTitles());
+        request.getRequestDispatcher("/WEB-INF/views/info.jsp").forward(request, response);
+    }
 }
 
 
